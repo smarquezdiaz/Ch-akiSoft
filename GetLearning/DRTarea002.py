6d869c3c6b29e71442fa7a5cff14334c9c3df50d
David Gregori Rodriguez Calle <www.str_hpl@hotmail.com>
2025-07-25 19:44:34 -0400

    Subiendo test case 400 y 404

--- GetLearning/DRTarea002.py
+++ GetLearning/DRTarea002.py
@@ -1,18 +1,6 @@
 import pytest
 import requests
 
-
-@pytest.mark.smoke
-def US001_01_Obtener_los_datos_del_objeto_con_el_ID_1():
-    id = "1"
-    """El usuario debe obtener la información del objeto 1 como el año en el que ingreso al museo, su código interno, si es de dominio público, su id, su nombre, su link de wikidata, su rol, el nombre del artista quien lo creo, la nacionalidad del artista, la fecha de nacimiento y muerte del artista"""
-    url = "https://collectionapi.metmuseum.org/public/collection/v1/objects/"
-    list_url = url + id
-    response = requests.get(list_url)
-    assert response.status_code == 200
-    """prioridad media, resultado esperado obtener una respuesta 200"""
-
-
 @pytest.mark.smoke
 def DR_Tarea_002_Obtener_el_error_404():
     """El usuario debe obtener un error 404 cuando solicita información de un objeto que no existe"""
@@ -46,4 +34,3 @@ def DR_Tarea_003_Obtener_el_error_400():
     response = requests.get(url)
     assert response.status_code == 400
     """prioridad media, resultado esperado obtener una respuesta 400"""
-
