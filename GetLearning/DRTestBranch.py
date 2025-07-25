96fa619fc47318708ac8a80bc279f1317a6d42eb
David Gregori Rodriguez Calle <www.str_hpl@hotmail.com>
2025-07-25 19:47:15 -0400

    subiendo test case 400 y 404

--- GetLearning/DRTestBranch.py
+++ GetLearning/DRTestBranch.py
@@ -13,37 +13,3 @@ def US001_01_Obtener_los_datos_del_objeto_con_el_ID_1():
     """prioridad media, resultado esperado obtener una respuesta 200"""
 
 
-@pytest.mark.smoke
-def DR_Tarea_002_Obtener_el_error_404():
-    """El usuario debe obtener un error 404 cuando solicita información de un objeto que no existe"""
-    object_id = "999999999"
-    """
-    pasos
-    1 entrar a postman
-    2 seleccionar get
-    3 llenar el object id con un id que no existe
-    4 clic en send
-    5 verificar que el estado esa 404
-    """
-    url = f"https://collectionapi.metmuseum.org/public/collection/v1/objects/{object_id}"
-    response = requests.get(url)
-    assert response.status_code == 404
-    """prioridad media, resultado esperado obtener una respuesta 404"""
-
-@pytest.mark.smoke
-def DR_Tarea_003_Obtener_el_error_400():
-    """El usuario debe obtener un error 400 cuando hace una petición mal formada ID no numérico"""
-    object_id = "abc"
-    """
-        pasos
-        1 entrar a postman
-        2 seleccionar get
-        3 llenar el object id con un id de solo letras
-        4 clic en send
-        5 verificar que el estado esa 400
-        """
-    url = f"https://collectionapi.metmuseum.org/public/collection/v1/objects/{object_id}"
-    response = requests.get(url)
-    assert response.status_code == 400
-    """prioridad media, resultado esperado obtener una respuesta 400"""
-
