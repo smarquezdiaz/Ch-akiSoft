4391b06d8c93a1b36732cae8dc0824384b6da269
Pie-De-Limon69 <choque2015li@gmail.com>
2025-07-25 20:02:17 -0400

    GC003-Test-case-code-error-400-405

---
+++ gitLearn/TestAutomatizacion400.py
@@ -0,0 +1,28 @@
+import pytest
+import requests
+
+@pytest.mark.negative
+def test_004_Verificar_error_404_al_acceder_a_endpoint_inexistente():
+    # Descripción: Verificar que el API responde con un código 404 al acceder a un recurso que no existe.
+    url = "https://collectionapi.metmuseum.org/public/collection/v1/departamentoss"
+    headers = {
+        'sec-ch-ua-full-version-list': '"Not)A;Brand";v="8.0.0.0", "Chromium";v="138.0.7204.158", "Google Chrome";v="138.0.7204.158"',
+          'sec-ch-ua-platform': '"Windows"',
+          'Referer': 'https://www.youtube.com/',
+          'sec-ch-ua': '"Not)A;Brand";v="8", "Chromium";v="138", "Google Chrome";v="138"',
+          'sec-ch-ua-bitness': '"64"',
+          'sec-ch-ua-model': '""',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-form-factors': '"Desktop"',
+          'sec-ch-ua-wow64': '?0',
+          'sec-ch-ua-arch': '"x86"',
+          'sec-ch-ua-full-version': '"138.0.7204.158"',
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36',
+          'sec-ch-ua-platform-version': '"10.0.0"',
+          'Cookie': 'incap_ses_624_1662004=EukcHhTDh3VDj+La8uSoCCNtgWgAAAAAtNz6KHub1k0YYDYA4TsXsg==; visid_incap_1662004=VuGrvFLSQMirN4f4PQcF/SskgGgAAAAAQUIPAAAAAACLzR1LGkLsZyKc4KL3Cmvi'
+        }
+    response = requests.get(url, headers=headers)
+
+    assert response.status_code == 404
+
+#priodad : media
---
+++ gitLearn/TestAutomatizacion405.py
@@ -0,0 +1,12 @@
+import pytest
+import requests
+
+@pytest.mark.negative
+def test_003_Verificar_error_405_al_usar_método_incorrecto():
+    # Descripción: Verificar que el API del museo devuelve el código 405 cuando se utiliza el método POST en un endpoint que solo admite GET
+    url = "https://collectionapi.metmuseum.org/public/collection/v1/departments"
+    response = requests.post(url)
+
+    assert response.status_code == 405
+
+#priodad : media
